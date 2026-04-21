4baa8cc0b872216f84c518504316669a486d9ccf
JulietteOssandon <jossandon@acgp.cl>
2026-04-20 23:07:31 -0400

    arreglos

--- script.js
+++ script.js
@@ -206,3 +206,23 @@ if ('ontouchstart' in window) {
         revObs.observe(el);
     });
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  
+  const response = await fetch(form.action, {
+    method: "POST",
+    body: data,
+    headers: { 'Accept': 'application/json' }
+  });
+
+  if (response.ok) {
+    alert("¡Mensaje enviado con éxito!");
+    form.reset();
+  } else {
+    alert("Hubo un error al enviar el formulario.");
+  }
+});
